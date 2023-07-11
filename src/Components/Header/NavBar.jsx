a7365ad8cc45b0263cd53a66d58c7685b739aac0
Bodula-sravani <sravanibodula@gmail.com>
2023-07-11 23:38:05 +0530

    [Update] dropdown

--- src/Components/Header/NavBar.jsx
+++ src/Components/Header/NavBar.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { Profile } from "../Profile/Profile";
 import { Button } from "primereact/button";
+import { Link } from "react-router-dom";
 import { InputText } from "primereact/inputtext";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -17,23 +18,62 @@ export const NavBar = ({ onSearch, handleDestinationSearch }) => {
     <>
       <div className="header">
         <div className="container">
-          {!isMenuOpen ? (
-            <div className="logodiv">
-              <a href="/">
-                <h1 className="logo">XploreIN</h1>
-              </a>
-            </div>
-          ) : (
-            <div
-              className={`menu-toggle ${isMenuOpen ? "active" : ""}`}
-              onClick={handleMenuToggle}
-            >
-              <span></span>
-              <span></span>
-              <span></span>
+          <div className="logodiv">
+            <a href="/">
+              <h1 className="logo">XploreIN</h1>
+            </a>
+          </div>
+          <div
+            className={`menu-toggle ${isMenuOpen ? "active" : ""}`}
+            onClick={handleMenuToggle}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          {isMenuOpen && (
+            <div className="dropdownLeft">
+              <ul className="dropdown-menu">
+                <li className="dropdown-item">
+                  <Link to="/">
+                    <Button
+                      label="Home"
+                      icon="pi pi-home"
+                      className="listButtons"
+                    ></Button>
+                  </Link>
+                </li>
+                <li className="dropdown-item">
+                  <Link to="/ruraltourism">
+                    <Button
+                      label="India"
+                      icon="pi pi-compass"
+                      className="listButtons"
+                    ></Button>
+                  </Link>
+                </li>
+                <li className="dropdown-item">
+                  <Link to="/reviews">
+                    <Button
+                      label="Reviews"
+                      icon="pi pi-comments"
+                      className="listButtons"
+                    ></Button>
+                  </Link>
+                </li>
+                <li className="dropdown-item">
+                  <Link to="/#Footer">
+                    <Button
+                      label="Contact"
+                      icon="pi pi-phone"
+                      className="listButtons"
+                    ></Button>
+                  </Link>
+                </li>
+              </ul>
             </div>
           )}
-          <ul className={`nav-menu ${isMenuOpen ? "active" : ""}`}>
+          <ul className={"nav-menu"}>
             <li>
               <a href="/">Home</a>
             </li>
