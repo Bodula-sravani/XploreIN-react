ee3e5b102ee1d02a6dcc722195508995c12795bf
Bodula-sravani <sravanibodula@gmail.com>
2023-07-11 12:17:40 +0530

    [Update] places responsive

--- src/Components/PlaceCategories/Places.jsx
+++ src/Components/PlaceCategories/Places.jsx
@@ -1,53 +1,3 @@
-// import React from "react";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import mumbai from "../../assets/mumbai.jpg";
-// import Hyderabad from "../../assets/Hyderabad.jpg";
-// import goa from "../../assets/goa.jpg";
-// import chennai from "../../assets/chennai.jpg";
-// import newDelhi from "../../assets/newDelhi.jpg";
-
-// import "./Places.css";
-
-// const places = [mumbai, Hyderabad, chennai, newDelhi, goa];
-// const placesName = ["Mumbai", "Hyderabad", "Chennai", "New Delhi", "Goa"];
-// export const Places = () => {
-//   const settings = {
-//     dots: false,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 4,
-//     slidesToScroll: 1,
-//     centerMode: true,
-//     centerPadding: "0px",
-//   };
-
-//   const renderImages = () => {
-//     return places.map((place, index) => (
-//       <div key={index}>
-//         <div className="shadow slide-item transition-all">
-//           <img
-//             src={place}
-//             className="object-cover h-48 rounded"
-//             style={{ borderRadius: "10px" }}
-//           />
-//         </div>
-//         <div className="name-overlay">
-//           <p className="name-text">{placesName[index]}</p>
-//         </div>
-//       </div>
-//     ));
-//   };
-//   return (
-//     <div className="flex items-center justify-center myCarousel">
-//       <div className="w-full">
-//         <Slider {...settings}>{renderImages()}</Slider>
-//       </div>
-//     </div>
-//   );
-// };
-
 import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -99,6 +49,22 @@ const placesName = [
   "Mysore",
 ];
 
+const placesDescription = [
+  "Mumbai is a bustling city known for its vibrant culture, Bollywood film industry, and iconic landmarks like the Gateway of India.",
+  "Hyderabad, the 'City of Pearls,' offers a rich blend of history and modernity, with its magnificent architecture, delicious biryani, and charming Old City.",
+  "Chennai, the capital of Tamil Nadu, is renowned for its beautiful beaches, classical music, and rich cultural heritage.",
+  "New Delhi, the capital of India, showcases a mix of ancient and modern attractions, including historical monuments, bustling markets, and political landmarks.",
+  "Goa is a popular beach destination famous for its pristine sandy shores, vibrant nightlife, and Portuguese-influenced architecture.",
+  "Jaipur, the 'Pink City,' captivates visitors with its magnificent palaces, bustling bazaars, and royal heritage of Rajputana.",
+  "Agra is home to the awe-inspiring Taj Mahal, a UNESCO World Heritage Site and one of the Seven Wonders of the World.",
+  "Udaipur, often called the 'City of Lakes,' is renowned for its enchanting palaces, scenic lakes, and romantic ambiance.",
+  "Kerala, known as 'God's Own Country,' offers breathtaking backwaters, serene beaches, lush greenery, and Ayurvedic treatments.",
+  "Varanasi, situated on the banks of the sacred Ganges River, is a spiritual city with ancient temples, ghats, and mesmerizing Ganga Aarti.",
+  "Shimla, nestled in the Himalayas, is a popular hill station known for its colonial architecture, scenic landscapes, and pleasant weather.",
+  "Amritsar is home to the iconic Golden Temple, a spiritual and cultural center for Sikhs, and the vibrant Wagah Border ceremony.",
+  "Mysore, with its magnificent Mysore Palace, silk sarees, and rich cultural heritage, is a city of grandeur and royal charm.",
+];
+
 const CustomPrevArrow = (props) => {
   const { className, style, onClick } = props;
   return (
@@ -126,7 +92,7 @@ export const Places = () => {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    // slidesToShow: 2,
     slidesToScroll: 1,
     centerMode: true,
     centerPadding: "0px",
@@ -136,12 +102,13 @@ export const Places = () => {
 
   const renderImages = () => {
     return places.map((place, index) => (
-      <div key={index}>
-        <div className="shadow slide-item transition-all">
+      <div key={index} className="slide-item">
+        <div className="image-wrapper">
           <img src={place} className="object-cover h-48 rounded imageStyle" />
-        </div>
-        <div className="name-overlay">
-          <p className="name-text">{placesName[index]}</p>
+          <div className="name-overlay">
+            <p className="name-text">{placesName[index]}</p>
+            <p className="name-text">{placesDescription[index]}</p>
+          </div>
         </div>
       </div>
     ));
