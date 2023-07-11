3abdf2f83938e5c21225900312c449224fa6acf9
Bodula-sravani <sravanibodula@gmail.com>
2023-07-11 23:39:10 +0530

    [Update] extra info

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -18,42 +18,47 @@ export const Reviews = () => {
             Authorization: `Bearer ${token}`,
           },
         });
-
-        //   if (response.status === "ok") {
         const data = await response.json();
         setReviews([...data]);
         console.log(data);
       } catch (error) {
         setMessage("Please Login to View the Reviews");
       }
-      //   }
     };
     fetchData();
   }, []);
   console.log(message);
   console.log(reviews);
   return (
-    <div className="reviews">
-      {!reviews && <p>{message}</p>}
-      {reviews &&
-        reviews.map((r) => (
-          <Card
-            title={
-              <div className="CardTitle">
-                <div className="userBar">
-                  <i className="pi pi-user"></i>
-                  <p>{r.user.userName}</p>
+    <div className="head">
+      <h1 className="title">User Reviews</h1>
+      <div className="reviews">
+        {!reviews && <p>{message}</p>}
+        {reviews &&
+          reviews.map((r) => (
+            <Card
+              title={
+                <div className="CardTitle">
+                  <div className="userBar">
+                    <i className="pi pi-user"></i>
+                    <p>{r.user.userName}</p>
+                  </div>
+                  {r.destination}
+                  <Rating value={r.rating} cancel={false} />
                 </div>
-                {r.destination}
-                <Rating value={r.rating} cancel={false} />
-              </div>
-            }
-            className="Card"
-          >
-            <p>{r.description}</p>
-            <p></p>
-          </Card>
-        ))}
+              }
+              className="Card"
+            >
+              <p>{r.description}</p>
+              <p></p>
+            </Card>
+          ))}
+      </div>
+      <hr></hr>
+      <p>
+        These are some of the user experiences about their trips. More features
+        to be added soon...
+      </p>
     </div>
   );
 };
