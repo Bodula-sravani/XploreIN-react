a72846f956be3539d8a8476ca7f40626e8719808
Bodula-sravani <sravanibodula@gmail.com>
2023-07-12 22:39:01 +0530

    [Update] styles

--- src/Components/Api/GetLocationData.jsx
+++ src/Components/Api/GetLocationData.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import DisplayCard from "../DisplayCardComponent/DisplayCard";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Loader1 } from "../Loader/Loader1";
+import { height } from "@mui/system";
 export const GetLocationResturants = ({ type, lat, lon }) => {
   const [places, setPlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       const url = `https://travel-advisor.p.rapidapi.com/${type}/list-by-latlng?latitude=${lat}&longitude=${lon}&lang=en_US&units=km`;
@@ -21,6 +24,7 @@ export const GetLocationResturants = ({ type, lat, lon }) => {
         const result = await response.json();
 
         setPlaces(result.data);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -67,22 +71,64 @@ export const GetLocationResturants = ({ type, lat, lon }) => {
     if (place.phone) count++;
     return count;
   }
+  const [slidesToShow, setSlidesToShow] = useState(2); // Initial number of slides to show
 
-  return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div style={{ width: "700px", height: "700px" }}>
+  useEffect(() => {
+    const handleResize = () => {
+      // Update the number of slides to show based on the window width
+      const windowWidth = window.innerWidth;
+      if (windowWidth < 600) {
+        setSlidesToShow(1);
+      } else if (windowWidth < 960) {
+        setSlidesToShow(1);
+      } else {
+        setSlidesToShow(2);
+      }
+    };
+
+    // Attach the resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call the handleResize function initially
+    handleResize();
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="load1Center">
+        <Loader1 />
+      </div>
+    );
+  } else {
+    return (
+      <div key={type} className="carouselDiv">
         <Carousel
-          key={type}
-          showThumbs={false}
-          style={{ width: "700px", height: "700px !important" }}
+          infiniteLoop
+          showStatus={false}
+          showArrows
+          interval={2000}
+          swipeable
+          emulateTouch
+          showIndicators={false}
+          centerMode
+          centerSlidePercentage={100 / slidesToShow}
         >
           {displayedPlaces.map((place) => (
-            <div key={place.id}>
+            <div
+              className="slide-item"
+              key={place.id}
+              style={{ height: "700px", width: "500px" }}
+            >
               <DisplayCard type={type} place={place} />
             </div>
           ))}
         </Carousel>
       </div>
-    </div>
-  );
+    );
+  }
 };
